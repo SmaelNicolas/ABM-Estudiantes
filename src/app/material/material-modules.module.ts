54609a44a89680b389a7ece246bfc219dd104582
Smael Nicolas <nico.smael@gmail.com>
2022-04-06 12:49:37 -0300

    material modules

---
+++ src/app/material/material-modules.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatChipsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatTableModule,
+  ],
+})
+export class MaterialModulesModule {}
