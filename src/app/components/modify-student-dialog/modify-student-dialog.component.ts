e071c2bbcb694a19d707f27ea14ae2b9c5a3f597
Smael Nicolas <nico.smael@gmail.com>
2022-04-06 12:50:00 -0300

    modify student dialog

---
+++ src/app/components/modify-student-dialog/modify-student-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-student-dialog',
+  templateUrl: './modify-student-dialog.component.html',
+  styleUrls: ['./modify-student-dialog.component.css']
+})
+export class ModifyStudentDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
