84faaf9175b8421f109d246dd210181da1b614d2
Smael Nicolas <nico.smael@gmail.com>
2022-04-06 12:50:55 -0300

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,67 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ProyectoFinal';
+
+  showCreateStudent: boolean = false;
+  showDeleteStudent: boolean = false;
+  showModifyStudent: boolean = false;
+  showListStudents: boolean = false;
+  showListCourses: boolean = false;
+  showLogin: boolean = false;
+
+  constructor() {}
+
+  showCreateStudentHandler() {
+    this.showCreateStudent = true;
+    this.showDeleteStudent = false;
+    this.showModifyStudent = false;
+    this.showListStudents = false;
+    this.showListCourses = false;
+    this.showLogin = false;
+    console.log('LLAMA');
+  }
+  showDeleteStudentHandler() {
+    this.showCreateStudent = false;
+    this.showDeleteStudent = true;
+    this.showModifyStudent = false;
+    this.showListStudents = false;
+    this.showListCourses = false;
+    this.showLogin = false;
+  }
+  showModifyStudentHandler() {
+    this.showCreateStudent = false;
+    this.showDeleteStudent = false;
+    this.showModifyStudent = true;
+    this.showListStudents = false;
+    this.showListCourses = false;
+    this.showLogin = false;
+  }
+  showListStudentsHandler() {
+    this.showCreateStudent = false;
+    this.showDeleteStudent = false;
+    this.showModifyStudent = false;
+    this.showListStudents = true;
+    this.showListCourses = false;
+    this.showLogin = false;
+  }
+  showListCoursesHandler() {
+    this.showCreateStudent = false;
+    this.showDeleteStudent = false;
+    this.showModifyStudent = false;
+    this.showListStudents = false;
+    this.showListCourses = true;
+    this.showLogin = false;
+  }
+  showLoginHandler() {
+    this.showCreateStudent = false;
+    this.showDeleteStudent = false;
+    this.showModifyStudent = false;
+    this.showListStudents = false;
+    this.showListCourses = false;
+    this.showLogin = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModulesModule } from './material/material-modules.module';
+import { NavbarLateralComponent } from './components/navbar-lateral/navbar-lateral.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CreateStudentComponent } from './components/create-student/create-student.component';
+import { DeleteStudentComponent } from './components/delete-student/delete-student.component';
+import { ListStudentsComponent } from './components/list-students/list-students.component';
+import { ListCoursesComponent } from './components/list-courses/list-courses.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DeleteStudentDialogComponent } from './components/delete-student-dialog/delete-student-dialog.component';
+import { ModifyStudentComponent } from './components/modify-student/modify-student.component';
+import { ModifyStudentDialogComponent } from './components/modify-student-dialog/modify-student-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarLateralComponent,
+    ToolbarComponent,
+    CreateStudentComponent,
+    DeleteStudentComponent,
+    ListStudentsComponent,
+    ListCoursesComponent,
+    LoginComponent,
+    DeleteStudentDialogComponent,
+    ModifyStudentComponent,
+    ModifyStudentDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModulesModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
