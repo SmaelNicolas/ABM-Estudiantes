99e1504ca86dbb1463ea924409f238dc3c7328cf
Smael Nicolas <nico.smael@gmail.com>
2022-04-08 12:44:21 -0300

    props to handle users

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -13,25 +13,39 @@ import {
 })
 export class LoginComponent implements OnInit {
   title: string = 'Login';
-
   loginForm!: FormGroup;
+  start: boolean = true;
+  clickStudent: boolean = false;
+  clickAdmin: boolean = false;
+  // logInStudent: boolean = false;
 
-  //array para ver la lista de estudiantes/administradores logeados
-  loggedList: any[] = [{}];
+  @Input() showNavbar: any;
+  @Input() showLoading: any;
+  @Input() showLogin: any;
+  @Input() logInStudent: any;
+
+  @Output()
+  showNavbarChange = new EventEmitter<any>();
+  @Output()
+  showLoadingChange = new EventEmitter<any>();
+  @Output()
+  showLoginChange = new EventEmitter<any>();
+  @Output()
+  logInStudentChange = new EventEmitter<any>();
 
   constructor(public lf: FormBuilder) {
     this.loginForm = this.lf.group({
-      password: new FormControl('', [
-        Validators.required,
-        this.noWhitespaceValidator,
-        Validators.minLength(5),
-        Validators.maxLength(15),
-      ]),
       email: new FormControl('', [
         Validators.required,
         this.noWhitespaceValidator,
         Validators.email,
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        this.noWhitespaceValidator,
+        Validators.minLength(8),
+        Validators.maxLength(20),
       ]),
     });
   }
@@ -44,7 +58,33 @@ export class LoginComponent implements OnInit {
     return isValid ? null : { whitespace: true };
   }
 
+  public clickStudentHandler() {
+    this.clickStudent = true;
+    this.clickAdmin = false;
+  }
+
+  public clickAdminHandler() {
+    this.clickStudent = false;
+    this.clickAdmin = true;
+  }
+
+  showNavbarHandler() {
+    this.showNavbarChange.emit(true);
+  }
+
   public logUser() {
-    //chekear si el usuario existe en la base de datos
+    if (this.clickStudent) {
+      this.logInStudentChange.emit(true);
+    }
+    this.showNavbarHandler();
+    this.showLoginChange.emit(false);
+  }
+
+  showLoadingHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
+    this.start = false;
   }
 }
