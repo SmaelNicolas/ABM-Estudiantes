2a4a111288149b09e179008b47acbc0f4f1a2eb2
Smael Nicolas <nico.smael@gmail.com>
2022-04-07 23:08:47 -0300

    new @inputs for loading & modify courses

--- src/app/components/navbar-lateral/navbar-lateral.component.ts
+++ src/app/components/navbar-lateral/navbar-lateral.component.ts
@@ -8,13 +8,16 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 export class NavbarLateralComponent implements OnInit {
   events: string[] = [];
   opened: boolean = false;
+  start: boolean = true;
 
   @Input() showCreateStudent: any;
   @Input() showDeleteStudent: any;
   @Input() showModifyStudent: any;
+  @Input() showModifyCourses: any;
   @Input() showListStudents: any;
   @Input() showListCourses: any;
   @Input() showLogin: any;
+  @Input() showLoading: any;
 
   @Output()
   showCreateStudentChange = new EventEmitter<any>();
@@ -23,11 +26,15 @@ export class NavbarLateralComponent implements OnInit {
   @Output()
   showModifyStudentChange = new EventEmitter<any>();
   @Output()
+  showModifyCoursesChange = new EventEmitter<any>();
+  @Output()
   showListStudentsChange = new EventEmitter<any>();
   @Output()
   showListCoursesChange = new EventEmitter<any>();
   @Output()
   showLoginChange = new EventEmitter<any>();
+  @Output()
+  showLoadingChange = new EventEmitter<any>();
 
   constructor() {}
 
@@ -41,6 +48,10 @@ export class NavbarLateralComponent implements OnInit {
   }
 
   showCreateStudentHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(true);
     this.showDeleteStudentChange.emit(false);
     this.showModifyStudentChange.emit(false);
@@ -50,43 +61,82 @@ export class NavbarLateralComponent implements OnInit {
   }
 
   showDeleteStudentHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(false);
     this.showDeleteStudentChange.emit(true);
     this.showModifyStudentChange.emit(false);
+    this.showModifyCoursesChange.emit(false);
     this.showListStudentsChange.emit(false);
     this.showListCoursesChange.emit(false);
     this.showLoginChange.emit(false);
   }
   showModifyStudentHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(false);
     this.showDeleteStudentChange.emit(false);
     this.showModifyStudentChange.emit(true);
+    this.showModifyCoursesChange.emit(false);
+    this.showListStudentsChange.emit(false);
+    this.showListCoursesChange.emit(false);
+    this.showLoginChange.emit(false);
+  }
+  showModifyCoursesHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
+    this.showCreateStudentChange.emit(false);
+    this.showDeleteStudentChange.emit(false);
+    this.showModifyStudentChange.emit(false);
+    this.showModifyCoursesChange.emit(true);
     this.showListStudentsChange.emit(false);
     this.showListCoursesChange.emit(false);
     this.showLoginChange.emit(false);
   }
   showListStudentsHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(false);
     this.showDeleteStudentChange.emit(false);
     this.showModifyStudentChange.emit(false);
+    this.showModifyCoursesChange.emit(false);
     this.showListStudentsChange.emit(true);
     this.showListCoursesChange.emit(false);
     this.showLoginChange.emit(false);
   }
   showListCoursesHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(false);
     this.showDeleteStudentChange.emit(false);
     this.showModifyStudentChange.emit(false);
+    this.showModifyCoursesChange.emit(false);
     this.showListStudentsChange.emit(false);
     this.showListCoursesChange.emit(true);
     this.showLoginChange.emit(false);
   }
   showLoginHandler() {
+    this.showLoadingChange.emit(true);
+    setTimeout(() => {
+      this.showLoadingChange.emit(false);
+    }, 1500);
     this.showCreateStudentChange.emit(false);
     this.showDeleteStudentChange.emit(false);
     this.showModifyStudentChange.emit(false);
+    this.showModifyCoursesChange.emit(false);
     this.showListStudentsChange.emit(false);
     this.showListCoursesChange.emit(false);
     this.showLoginChange.emit(true);
+    this.start = false;
   }
 }
