5a9f7f96f8b441ae8c72edb22d68cb3d06af0e42
Smael Nicolas <nico.smael@gmail.com>
2022-04-16 19:50:20 -0300

    student service

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -1,9 +1,91 @@
 import { Injectable } from '@angular/core';
+import { Observable, map, of } from 'rxjs';
+import { Courses } from '../class/courses';
+import { Student } from '../class/student';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentsService {
+  studentList: Student[] = [
+    {
+      id: 11111111,
+      name: 'Nicolas',
+      lastName: 'Smael',
+      email: 'Smael@Nicolas.com',
+      password: '11111111',
+      courses: ['angular', 'javascript', 'react'],
+    },
+    {
+      id: 11111112,
+      name: 'Fernando',
+      lastName: 'Olvera',
+      email: 'Olvera@Fernando.com',
+      password: '11111111',
+      courses: ['angular', 'javascript'],
+    },
+    {
+      id: 11111113,
+      name: 'Abner',
+      lastName: 'García',
+      email: 'García@Abner.com',
+      password: '11111111',
+      courses: ['angular'],
+    },
+    {
+      id: 11111114,
+      name: 'Victoria',
+      lastName: 'Cordero',
+      email: 'Cordero@Victoria.com',
+      password: '11111111',
+      courses: ['react'],
+    },
+    {
+      id: 11111115,
+      name: 'Pablo',
+      lastName: 'García',
+      email: 'García@Pablo.com',
+      password: '11111111',
+      courses: [],
+    },
+  ];
 
-  constructor() { }
+  studentList$!: Observable<Student[]>;
+
+  constructor() {
+    this.studentList$ = new Observable((suscription) => {
+      suscription.next(this.studentList);
+    });
+  }
+
+  getStudentList(): Observable<Student[]> {
+    return this.studentList$;
+  }
+
+  addStudent(student: Student): void {
+    this.studentList.push(student);
+  }
+
+  deleteStudent(id: number): void {
+    this.studentList = this.studentList.filter((student) => student.id !== id);
+  }
+
+  getStudent(id: number): Observable<Student> {
+    return this.studentList$.pipe(
+      map((studentList) => studentList.find((student) => student.id === id)!)
+    );
+  }
+
+  alreadyStudent(id: number): Observable<boolean> {
+    return of(this.studentList.some((student) => student.id === id));
+  }
+
+  replaceStudent(student: Student): void {
+    const index = this.studentList.findIndex((s) => s.id === student.id);
+    this.studentList[index] = student;
+  }
+
+  studentIsInCourse(student: Student, course: string): Observable<boolean> {
+    return of(student.courses.includes(course));
+  }
 }
