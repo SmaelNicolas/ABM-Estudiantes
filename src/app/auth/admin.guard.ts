e5803685eef26c737ae179ff49ddb44b165676b8
Smael Nicolas <nico.smael@gmail.com>
2022-05-01 21:26:37 -0300

    fire alert

--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -7,18 +7,19 @@ import {
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AutenticacionService } from '../services/auth.service';
+import { AutenticacionService } from '../services/admin.service';
+import Swal from 'sweetalert2/dist/sweetalert2.all.js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private autenticacion: AutenticacionService) {}
+  constructor(
+    private autenticacion: AutenticacionService,
+    private router: Router
+  ) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
+  canActivate():
     | boolean
     | UrlTree
     | Observable<boolean | UrlTree>
@@ -26,7 +27,18 @@ export class AdminGuard implements CanActivate {
     if (this.autenticacion.getUserIn().rol === 'admin') {
       return true;
     } else {
-      alert('No tiene acceso a este contenido');
+      Swal.fire({
+        title: 'No tenes permisos para acceder',
+        text: 'Queres logearte como Admin?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Log In',
+        cancelButtonText: 'Cancel',
+      }).then((result) => {
+        if (result.value) {
+          this.router.navigate(['login']);
+        }
+      });
       return false;
     }
   }
