be0cfc030ed72da5000c084aefe9470af42b0997
Smael Nicolas <nico.smael@gmail.com>
2022-05-05 18:27:49 -0300

    remove prop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'ProyectoFinal';
   router!: string;
 
   constructor(private _router: Router) {}
