98f0b25dabdb118eb5759253fcd11eec2d195d27
Smael Nicolas <nico.smael@gmail.com>
2022-05-05 17:06:49 -0300

    new interfaces

---
+++ src/app/interfaces/course.ts
@@ -0,0 +1,7 @@
+export interface CourseInterface {
+  name: string;
+  description: string;
+  imageUrl: string;
+  isAvailable: boolean;
+  id: number | undefined;
+}
---
+++ src/app/interfaces/student.ts
@@ -0,0 +1,10 @@
+export interface StudentInterface {
+  id: number | undefined;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  courses: string[];
+  rol: string;
+  dni: number | undefined;
+}
